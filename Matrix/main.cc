93a8c3fe4bbdf22feebac63256251664ca73d2de
zbrze <58784685+zbrze@users.noreply.github.com>
2021-04-04 13:54:29 +0200

    Update main.cc

--- Matrix/main.cc
+++ Matrix/main.cc
@@ -5,18 +5,6 @@
 #include "utils/argumentParsing.h"
 
 
-/** Tresc zadan dla Panstwa:
-
-Zadanie 0: absolutnie obowiazkowe, chociaz bez punktow
-1) Pierwsza rzecza jest poprawa bledow kompilacji, czyli wpisanie poprawnych Panstwa danych w pliku: programmerDetails.h
-2) Oddanne kolokwium musi sie bezwzglednie kompilowac, jesli sie nie skompiluje to jest to 0 punktow za kolokwium!
-   Oczywiscie w razie problemow z kompilacja prosze pisać
-3) Dobrze, jesli nie byloby warningow kompilacji, ale za to nie obnizam punktow.
-4) Proszę aby się program nie crashował podczas testów, jesli ktoś nie umie zrobić danego zagadnienia, to przynajmniej żeby program się nie wywalał.
-5) Mam program antyplagiatowy, dlatego prosze pracowac samodzielnie! Osoby ktore udostepniaja swoje rozwiazania rowniez beda mialy kare!
-
-Zadanie 1: patrz plik: matrix.h
-**/
 
 int main(int argc, char* argv[])
 {
